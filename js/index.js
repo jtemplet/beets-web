10b4f521f091ca934aa0ff0cc26f9ce24223d40f
Jason Templeton <jtempleton@acemetrix.com>
2015-01-28 09:25:05 -0800

    Added Backbone.history and navigate

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 //var ShellView = require('./views/shell');
 //
 //var shellView = new ShellView();
+var Backbone = require('backbone');
 var Router = require('./routers/router');
 
-new Router();
+var router = new Router();
+Backbone.history.start();
+router.navigate('', {trigger: true});
