d8fbb13a179c11603dbf704fce86e84425db97d0
Jason Templeton <jtempleton@acemetrix.com>
2015-02-09 10:04:35 -0800

    Added recentEvents timeline

--- app/js/views/recentEvents.js
+++ app/js/views/recentEvents.js
@@ -1,17 +1,36 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var eventTemplate = require('../templates/recentEvent.hbs');
 var template = require('../templates/recentEvents.hbs');
+
 Backbone.$ = $;
 
 module.exports = Backbone.View.extend({
+
+  el: $('body'),   /* This is crucial  */
 
   initialize: function(){
     console.log('recentEventsView::initialize()');
   },
 
+  events: {
+    "click a#recentEventsLink": "viewRecentEvents"
+  },
+
   render: function(){
     console.log('recentEventsView::render()');
     $('#dashboard-content').html(template());
+
+    // Get all the events
+    //$('.timeline').html(eventTemplate());
     return this;
+  },
+
+  viewRecentEvents: function(e) {
+    e.preventDefault();
+    console.log('****** REV::viewREV *****');
+    e.stopPropagation();
+
+    this.render();
   }
 });
